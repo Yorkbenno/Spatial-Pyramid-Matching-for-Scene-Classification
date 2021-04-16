b5afe2cbafdf6e5a575052dda0d4bf0bd609b5a4
Yorkbenno <1007368294@qq.com>
2021-04-17 00:19:55 +0800

    constructed self-designed simulation of VGG

--- code/network_layers.py
+++ code/network_layers.py
@@ -1,8 +1,10 @@
 import numpy as np
 import scipy.ndimage
-import os,time
+import os, time
+import skimage
 
-def extract_deep_feature(x,vgg16_weights):
+
+def extract_deep_feature(x, vgg16_weights):
 	'''
 	Extracts deep features from the given VGG-16 weights.
 
@@ -13,11 +15,40 @@ def extract_deep_feature(x,vgg16_weights):
 	[output]
 	* feat: numpy.ndarray of shape (K)
 	'''
-	pass
+
+	mean = [0.485, 0.456, 0.406]
+	std = [0.229, 0.224, 0.225]
+	x = skimage.transform.resize(x, (224, 224))
+	c = x.shape[2]
+	if c == 1:
+		x = np.matlib.repmat(x, 1, 1, 3)
+	if c == 4:  # Weird, but found this error while processing
+		x = x[:, :, 0:3]
+	for i in range(3):
+		x[:, :, i] = (x[:, :, i] - mean[i]) / std[i]
+
+	L = len(vgg16_weights)
+	linear = 0
+	for l in range(L):
+		params = vgg16_weights[l]
+		if params[0] == 'conv2d':
+			x = multichannel_conv2d(x, params[1], params[2])
+		elif params[0] == 'relu':
+			x = relu(x)
+		elif params[0] == 'maxpool2d':
+			x = max_pool2d(x, params[1])
+		elif params[0] == 'linear':
+			x = linear(x, params[1], params[2])
+			linear += 1
+			if linear == 2:
+				break
+
+	feat = x.reshape(1, np.shape(x)[0])
+	return feat
 
 
-def multichannel_conv2d(x,weight,bias):
-	'''
+def multichannel_conv2d(x, weight, bias):
+    '''
 	Performs multi-channel 2D convolution.
 
 	[input]
@@ -28,11 +59,30 @@ def multichannel_conv2d(x,weight,bias):
 	[output]
 	* feat: numpy.ndarray of shape (H,W,output_dim)
 	'''
-	
-	pass
+
+    H, W, input_dim = x.shape
+    output_dim, input_dim, kernel_size, kernel_size = weight.shape
+    feat = []
+
+    for i in range(output_dim):
+        value = np.zeros((H, W, 1))
+
+        for j in range(input_dim):
+            filter = weight[i, j, :]
+            filter = np.flip(filter)
+            channel = x[:, :, j]
+            value[:, :, 0] += scipy.ndimage.convolve(channel, filter, mode='constant')
+
+        value += bias[i]
+        feat.append(value)
+
+    feat = np.dstack(feat)
+
+    return feat
+
 
 def relu(x):
-	'''
+    '''
 	Rectified linear unit.
 
 	[input]
@@ -41,10 +91,13 @@ def relu(x):
 	[output]
 	* y: numpy.ndarray
 	'''
-	pass
 
-def max_pool2d(x,size):
-	'''
+    y = np.maximum(0, x)
+    return y
+
+
+def max_pool2d(x, size):
+    '''
 	2D max pooling operation.
 
 	[input]
@@ -54,11 +107,17 @@ def max_pool2d(x,size):
 	[output]
 	* y: numpy.ndarray of shape (H/size,W/size,input_dim)
 	'''
-	
-	pass
 
-def linear(x,W,b):
-	'''
+    H, W, input_dim = x.shape
+    H_after = int(H / size)
+    W_after = int(W / size)
+
+    y = x.reshape(H_after, size, W_after, size, input_dim).max(axis=(1, 3))
+    return y
+
+
+def linear(x, W, b):
+    '''
 	Fully-connected layer.
 
 	[input]
@@ -69,5 +128,6 @@ def linear(x,W,b):
 	[output]
 	* y: numpy.ndarray of shape (output_dim)
 	'''
-	pass
-
+    # special case in first layer
+    y = W @ x.T.flatten() + b
+    return y
--- code/test.py
+++ code/test.py
@@ -1,7 +1,16 @@
 import numpy as np
 
 if __name__ == '__main__':
-    arr = np.ones((2, 3))
-    print(2.0**(-2))
-    arr2 = np.zeros((1, 3))
-
+    arr = np.arange(9).reshape((3, 3, 1))
+    ar2 = np.arange(9).reshape((3, 3, 1)) * 2
+    # print(arr)
+    # print(np.max(arr, axis=0))
+    # print(np.max(arr, axis=1))
+    # print(np.max(arr, axis=2))
+    # print(np.max(arr, axis=(0, 1)))
+    # print(np.max(arr, axis=(1, 2)))
+    # a = np.arange(9).reshape(3, 3)
+    # print(np.flip(a))
+    # print(np.flipud(np.fliplr(a)))
+    print(np.stack((arr, ar2), axis=-1).shape)
+    print(np.dstack((arr, ar2)).shape)
